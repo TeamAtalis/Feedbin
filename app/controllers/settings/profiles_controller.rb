1342fbf914bd13c6e41e1065019320042e3f7cae
SantiAtalis <santiago@atalisfunding.com>
2023-08-07 16:23:20 +0200

    Create a tags module that allows the admin to associate tags with profiles --> to implement the necessary logic on model and controller

--- app/controllers/settings/profiles_controller.rb
+++ app/controllers/settings/profiles_controller.rb
@@ -1,8 +1,13 @@
 class Settings::ProfilesController < ApplicationController
 
   def add_profile_to_user
-    Profile.find(params[:profile_id]).assign_profile_to_user(params[:user_id])
-    redirect_to settings_profiles_path
+    profile_id = params[:profile_id]
+    user_id = params[:user_id]
+
+    if(profile_id != "0")
+      Profile.find(profile_id).assign_profile_to_user(user_id)
+      redirect_to settings_profiles_path
+    end
   end
 
   def create_profile
---
+++ app/controllers/settings/tags_controller.rb
@@ -0,0 +1,39 @@
+class Settings::TagsController < ApplicationController
+
+    def add_tag_to_profile
+      profile_id = params[:profile_id]
+      tag_id = params[:tag_id]
+
+      if(profile_id == "0" || tag_id == "0")
+        # To implement a funcion that allows admin to add a tag to profile
+        redirect_to settings_tags_path
+      end
+    #   Profile.find(params[:profile_id]).assign_profile_to_user(params[:user_id])
+    #   redirect_to settings_profiles_path
+    end
+  
+    def create_tag
+    #   Profile.new(profile_name: params[:profile_name]).save
+    #   redirect_to settings_profiles_path
+    end
+    
+    def index
+      @tags = Tag.all
+      @profiles = Profile.all
+
+      store_location
+  
+      respond_to do |format|
+        format.html do
+          view = Settings::Tags::IndexView.new(
+            tags: @tags,
+            profiles: @profiles,
+          )
+          render view, layout: "settings"
+        end
+        # default index.js.erb
+        format.js {}
+      end  
+    end
+  end
+  
