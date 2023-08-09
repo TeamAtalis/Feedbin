df55edf76bc3ef7f0d60a814ce7d8d1ac0474933
MaciaAtalis <macia@atalisfunding.com>
2023-08-09 11:03:13 +0200

    [test:] Recover the r_users_profile_test.rb

--- app/models/r_users_profile.rb
+++ app/models/r_users_profile.rb
@@ -3,4 +3,5 @@ class RUsersProfile < ApplicationRecord
     belongs_to :user
 
     validates :profile_id, uniqueness: { scope: :user_id }
+    validates :user_id, presence: true
 end
--- test/models/r_users_profile_test.rb
+++ test/models/r_users_profile_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class RUsersProfileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :r_users_profiles
+
+  def setup
+    @aux_rup = RUsersProfile.find(1)
+  end
+
+  test "get profile" do
+    assert_not_nil @aux_rup
+  end
+
+  test "try insert profile with not complet atributes" do
+     rup = RUsersProfile.new()
+     assert_not rup.save, "Should not save the item without the profile_id and without user_id"
+  end
+
+  test "try insert the same profile to the same user" do
+    assert_not RUsersProfile.new(user_id: @aux_rup.user_id, profile_id: @aux_rup.profile_id).save, "Should not save because this relation already exists"
+  end
 end
