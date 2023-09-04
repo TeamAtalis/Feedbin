0c923875b38fa67114468fb5704191a181c19c75
SantiAtalis <santiago@atalisfunding.com>
2023-09-04 14:44:14 +0200

    [feat]: user edits own feeds; admin can edit profile feeds and own feeds

--- app/presenters/feed_presenter.rb
+++ app/presenters/feed_presenter.rb
@@ -1,25 +1,49 @@
 class FeedPresenter < BasePresenter
   presents :feed
 
-  def feed_link(link: nil, behavior: nil, &block)
-    args = [
-      link || @template.feed_entries_path(feed),
-      remote: true,
-      class: "feed-link",
-      data: {
-        behavior: behavior || "selectable show_entries open_item feed_link renamable user_title has_settings",
-        settings_path: @template.edit_subscription_path(feed, app: true),
-        feed_id: feed.id,
-        sourceable_target: "source",
-        action: "sourceable#selected",
-        sourceable_payload_param: feed.sourceable.to_h,
-        mark_read: {
-          type: "feed",
-          data: feed.id,
-          message: "Mark #{feed.title} as read?"
-        }.to_json
-      }
-    ]
+  def feed_link(link: nil, behavior: nil, has_profile: nil, &block)
+
+    args = []
+    
+    if @user.try(:admin?) || (!@user.try(:admin?) && !has_profile)
+      args = [
+        link || @template.feed_entries_path(feed),
+        remote: true,
+        class: "feed-link",
+        data: { 
+          behavior: behavior || "selectable show_entries open_item feed_link renamable user_title has_settings",
+          settings_path: @template.edit_subscription_path(feed, app: true),
+          feed_id: feed.id,
+          sourceable_target: "source",
+          action: "sourceable#selected",
+          sourceable_payload_param: feed.sourceable.to_h,
+          mark_read: {
+            type: "feed",
+            data: feed.id,
+            message: "Mark #{feed.title} as read?"
+          }.to_json
+        }
+      ]
+    else
+        args = [
+          link || @template.feed_entries_path(feed),
+          remote: true,
+          class: "feed-link",
+          data: {
+            behavior: behavior || "selectable show_entries open_item feed_link renamable user_title",
+            settings_path: @template.edit_subscription_path(feed, app: true),
+            feed_id: feed.id,
+            sourceable_target: "source",
+            action: "sourceable#selected",
+            sourceable_payload_param: feed.sourceable.to_h,
+            mark_read: {
+              type: "feed",
+              data: feed.id,
+              message: "Mark #{feed.title} as read?"
+            }.to_json
+          }
+        ]
+    end
     @template.link_to *args do
       yield
     end
