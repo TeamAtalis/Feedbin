d1835244317259084f96febe0c9ba373cd99cb0b
MaciaAtalis <macia@atalisfunding.com>
2023-09-28 11:50:02 +0200

    [feature]: install rbenv + ruby + bundle + rails

--- server-scripts/ruby_install.sh
+++ server-scripts/ruby_install.sh
@@ -1,42 +1,14 @@
 #!/bin/bash -i
-# -i flag is for run the script on interactive mode, if this flag is not there, the source ~/.bashrc will not works. [https://askubuntu.com/questions/64387/cannot-successfully-source-bashrc-from-a-shell-script] 
 
-# install dependecys for ruby
-sudo apt install git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
+# Install rbenv
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
-# install dependeces for rails
-sudo apt-get install nodejs
-
-# install dependences of some gems
-sudo apt-get install libpq-dev # For pg gem
-sudo apt-get install libldap2-dev # For idn-ruby
-sudo apt-get install libidn11-dev # For idn-ruby
-sudo apt-get install -y libvips
-
-# install rbenv for the ruby versions
-curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
-
-# export var for rbenv 
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-
-# reload the var 
-source ~/.bashrc
-
-# install ruby with specific version 
-rbenv install -v 3.2.2
-
-# set global and local version for ruby 
+# Install ruby 3.2.2
+rbenv install 3.2.2
 rbenv global 3.2.2
-rbenv local 3.2.2
 
-# install bunlde
 gem install bundler
-
-# reload gem instalation 
-source ~/.bashrc
-
-# install ruby on rails with rbenv
-gem install rails
-
-# install all gems for the project
-bundle install
+gem install rails 
