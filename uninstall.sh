d748783c7a8ba5cf6a270844be46b4ee424c555a
MaciaAtalis <macia@atalisfunding.com>
2023-10-03 09:57:58 +0200

    [clean]: clean unuseful files

--- uninstall.sh
+++
@@ -1,8 +0,0 @@
-# Define colors for the script
-Color_Off='\033[0m'       # Text Reset
-Green='\033[0;32m'        # Green
-Blue='\033[0;34m'         # Blue
-
-echo -e "${Green}Remove postgres${Color_Off}"
-sudo apt remove postgresql postgresql-contrib -y
-sudo apt autoremove
