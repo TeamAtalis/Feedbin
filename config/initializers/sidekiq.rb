143f6dfe44ec4cc9854dc6e10c1ed2c10c70f1fd
MarcAtalis <128678295+MarcAtalis@users.noreply.github.com>
2023-10-04 12:07:08 +0200

    add logger for Sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,6 +5,11 @@ SIDEKIQ_ALT = ConnectionPool.new(size: 1, timeout: 2) { Redis.new(timeout: 1.0) 
 
 Sidekiq.strict_args!(false)
 Sidekiq::Extensions.enable_delay!
+
+#Set up a Logger instance
+sidekiq_logger = Logger.new(File.join(Rails.root, 'log', 'sidekiq.log'))
+Sidekiq.logger = sidekiq_logger
+
 Sidekiq.configure_server do |config|
   ActiveRecord::Base.establish_connection
   config.server_middleware do |chain|
