db6d2940b34461232b93ba65d9a4765396e461a1
SantiAtalis <santiago@atalisfunding.com>
2023-10-06 20:16:30 +0200

    auto precompile on assets when the app is en development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.assets.js_compressor = nil
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   config.action_mailer.default_url_options = {host: ENV.fetch("DEFAULT_URL_OPTIONS_HOST", "feedbin.dev")}
 end
