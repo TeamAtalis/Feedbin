c400794a27545f5f6eedc48b375759181128aa79
MaciaAtalis <macia@atalisfunding.com>
2023-10-26 08:00:39 +0200

    [bugfix]: Benchmark now is returning USER-SYSTEM-TOTAL-REAL time

--- lib/benchmarks/my_benchmark.rb
+++ lib/benchmarks/my_benchmark.rb
@@ -5,20 +5,19 @@ class MyBenchmark
   #       rake task. This method will generate a report
   #       (admin_update_profile_rake_task.log) on /lib/benchmarks/reports.
   #
-  # input parameters: 
+  # input parameters:
   #       @params[:feed_id] [int]: id of the Feed.
   #       @params[:user_id] [int]: id of User. This user will be
   #                                the admin who makes the update
   #
   def self.admin_update_profile_rake_task(feed_id, user_id)
-    $stdout = File.new("#{Rails.root}/lib/benchmarks/reports/admin_update_profile_rake_task.log", 'a')
-    $stdout.sync = true
-    puts("***-----------------------------------------------------***\n#{Time.now}")
-    Benchmark.ips do |x|
-      x.report('admin_update_profile_rake_task') do
-        system("rake \"feedbin:update_profile[ #{feed_id}, #{user_id}]\" ")
-      end
-    end
-    puts("\n\n\n") # Add space between logs
+    # Measure the rake task
+    benchmark = Benchmark.measure { system("rake \"feedbin:update_profile[ #{feed_id}, #{user_id}]\" ") }
+    # Write Benchmark on log file
+    file = File.open("#{Rails.root}/lib/benchmarks/reports/admin_update_profile_rake_task.log", 'a')
+    file.puts("***-----------------------------------------------------***\n#{Time.now}")
+    file.puts("  USER_TIME  SYSTEM_TIME  TOTAL     (REAL)")
+    file.puts(benchmark, "\n\n")
+    file.close
   end
 end
