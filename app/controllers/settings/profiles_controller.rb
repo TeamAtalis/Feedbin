4a7d7d28f64cbbf595310e83921d82d3b937ddfd
SantiAtalis <santiago@atalisfunding.com>
2024-01-17 11:42:44 +0100

    [feat]: the admin can rename and delete profiles

--- app/controllers/settings/profiles_controller.rb
+++ app/controllers/settings/profiles_controller.rb
@@ -6,12 +6,12 @@ class Settings::ProfilesController < ApplicationController
 
     if(profile_id != "0")
       if(Profile.find(profile_id).assign_profile_to_user(user_id))
-        flash[:notice] = "Profile assigned to user successfully"
+        flash[:notice] = "Subscriptionm assigned to user successfully"
       else
-        flash[:alert] = "Profile already assigned to user"
+        flash[:alert] = "Subscriptionm already assigned to user"
       end
     else
-      flash[:alert] = "You must select a profile"
+      flash[:alert] = "You must select a subscription"
     end  
     redirect_to settings_profiles_path
   end
@@ -19,26 +19,34 @@ class Settings::ProfilesController < ApplicationController
   def create_profile
     if(!helpers.input_is_empty?(params[:profile_name].strip))
       Profile.new(profile_name: params[:profile_name]).save
-      flash[:notice] = "Profile created successfully"
+      flash[:notice] = "Subscription created successfully"
     else
-      flash[:alert] = "Profile name cannot be empty"
+      flash[:alert] = "Subscription name cannot be empty"
     end  
     redirect_to settings_profiles_path
   end
 
   def update_profile
-    puts params[:profile_name]
     if(!helpers.input_is_empty?(params[:profile_name].strip))
       Profile.find(params[:profile_id]).update(profile_name: params[:profile_name])
-      flash[:notice] = "Profile updated successfully"
+      flash[:notice] = "Subscription updated successfully"
     else
-      flash[:alert] = "Profile name cannot be empty"
+      flash[:alert] = "Subscription name cannot be empty"
     end
     redirect_to settings_profiles_path
   end
 
   def delete_profile
-    puts params[:profile_id]
+    if(RUsersProfile.where(profile_id: params[:profile_id]).count > 0)
+      RUsersProfile.where(profile_id: params[:profile_id]).destroy_all
+    end
+
+    if(RProfilesTag.where(profile_id: params[:profile_id]).count > 0)
+      RProfilesTag.where(profile_id: params[:profile_id]).destroy_all
+    end
+    Profile.find(params[:profile_id]).destroy
+    flash[:notice] = "Subscription deleted successfully"
+    redirect_to settings_profiles_path
   end
   
   def index
