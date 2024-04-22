40d0e497fd848ddb72e3a3505d95cbf5472fb208
MaciaAtalis <macia@atalisfunding.com>
2024-04-22 12:48:29 +0200

    [try]: try execute psql command

--- .docker_compose_scripts/init_web_service.sh
+++ .docker_compose_scripts/init_web_service.sh
@@ -3,7 +3,7 @@
 rails assets:precompile 
 
 RAILS_ENV=development rails db:create
-psql -h feedbin-postgres-1 --set ON_ERROR_STOP=1 --quiet --no-psqlrc --output /dev/null --file /app/db/structure.sql feedbin_development
+PGPASSWORD=$POSTGRES_PASSWORD psql -h feedbin-postgres-1  --set ON_ERROR_STOP=1 --quiet --no-psqlrc --output /dev/null --file /app/db/structure.sql feedbin_development
 RAILS_ENV=development rails db:migrate
 
 #rails db:seed
