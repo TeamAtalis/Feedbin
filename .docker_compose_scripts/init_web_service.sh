8a232e0f9a089a0c60d1a13cb7a67bc4d793d358
MaciaAtalis <macia@atalisfunding.com>
2024-04-23 07:58:42 +0200

    [feat]: Migrations + seed is working For run 'rails db:seed' REDIS_URL must be defined

--- .docker_compose_scripts/init_web_service.sh
+++ .docker_compose_scripts/init_web_service.sh
@@ -5,6 +5,6 @@
 RAILS_ENV=development rails db:create
 PGPASSWORD=$POSTGRES_PASSWORD psql -h postgres  --set ON_ERROR_STOP=1 --quiet --no-psqlrc --output /dev/null --file /app/db/structure.sql feedbin_development
 RAILS_ENV=development rails db:migrate
+RAILS_ENV=development rails db:seed
 
-#rails db:seed
 #rails s -b '0.0.0.0'
