b6d966ecff144b5b282f5ec16689a430a99d7193
MaciaAtalis <macia@atalisfunding.com>
2024-04-23 07:46:46 +0200

    [feat]: migrations are running

--- .docker_compose_scripts/init_web_service.sh
+++ .docker_compose_scripts/init_web_service.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-rails assets:precompile 
+#rails assets:precompile # is working 
 
 RAILS_ENV=development rails db:create
-PGPASSWORD=$POSTGRES_PASSWORD psql -h feedbin-postgres-1  --set ON_ERROR_STOP=1 --quiet --no-psqlrc --output /dev/null --file /app/db/structure.sql feedbin_development
+PGPASSWORD=$POSTGRES_PASSWORD psql -h postgres  --set ON_ERROR_STOP=1 --quiet --no-psqlrc --output /dev/null --file /app/db/structure.sql feedbin_development
 RAILS_ENV=development rails db:migrate
 
 #rails db:seed
