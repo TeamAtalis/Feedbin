1e2025218ad21268650ea57902da56f35dd23c77
MaciaAtalis <macia@atalisfunding.com>
2024-04-23 08:31:59 +0200

    [feat]: deployment is automated

--- .docker_compose_scripts/init_web_service.sh
+++ .docker_compose_scripts/init_web_service.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
-#rails assets:precompile # is working 
-
+# Create Backend
 RAILS_ENV=development rails db:create
 PGPASSWORD=$POSTGRES_PASSWORD psql -h postgres  --set ON_ERROR_STOP=1 --quiet --no-psqlrc --output /dev/null --file /app/db/structure.sql feedbin_development
 RAILS_ENV=development rails db:migrate
 RAILS_ENV=development rails db:seed
 
-#rails s -b '0.0.0.0'
+# Front-End
+RAILS_ENV=development rails assets:precompile
+
+# Run server
+RAILS_ENV=development rails s -b '0.0.0.0'
