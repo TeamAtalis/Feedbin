494a5f2f46b8ddb3caaad660ed6fb7ec2ec235c6
MarcAtalis <128678295+MarcAtalis@users.noreply.github.com>
2024-04-29 20:23:37 +0200

    [feat]: Fix Sidekiq & Search

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,23 +1,25 @@
 require "sidekiq"
-require ::File.expand_path("../../../lib/job_stat", __FILE__)
+require "connection_pool"
+require "redis"
 
-SIDEKIQ_ALT = ConnectionPool.new(size: 1, timeout: 2) { Redis.new(timeout: 1.0) }
+# Redis connection pool
+SIDEKIQ_ALT = ConnectionPool.new(size: 1, timeout: 2) { Redis.new(url: 'redis://redis:6379/0') }
 
+# Disable strict args check and enable delay extensions
 Sidekiq.strict_args!(false)
 Sidekiq::Extensions.enable_delay!
 
-#Set up a Logger instance
-sidekiq_logger = Logger.new(File.join(Rails.root, 'log', 'sidekiq.log'))
-Sidekiq.logger = sidekiq_logger
+# Set up Sidekiq logger to log to STDOUT
+Sidekiq.logger = Logger.new(STDOUT)
 
 Sidekiq.configure_server do |config|
+  # Establish ActiveRecord connection
   ActiveRecord::Base.establish_connection
-  config.server_middleware do |chain|
-    chain.add JobStat
-  end
-  config.redis = {id: "feedbin-server-#{::Process.pid}"}
+  # Set Redis connection for server
+  config.redis = { url: 'redis://redis:6379/0' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {id: "feedbin-client-#{::Process.pid}"}
+  # Set Redis connection for client
+  config.redis = { url: 'redis://redis:6379/0' }
 end
